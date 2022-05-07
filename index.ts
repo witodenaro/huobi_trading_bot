10d7936aa386b4acfbbcc33c22eb8f101dbb57ab
witodivaro <witodivaro@gmail.com>
2022-05-08 00:52:54 +0300

    feat: add egs

--- index.ts
+++ index.ts
@@ -2,14 +2,24 @@ import { initSocketConnection } from "./src/connection/sockets";
 
 import AncOrderFeedee from "./src/feedees/orders/ancOrder";
 import BtcOrderFeedee from "./src/feedees/orders/btcOrder";
+import EgsOrderFeedee from "./src/feedees/orders/egsOrder";
+
 import AncPriceFeedee from "./src/feedees/prices/ancPrice";
 import BtcPriceFeedee from "./src/feedees/prices/btcPrice";
+import EgsPriceFeedee from "./src/feedees/prices/egsPrice";
 
 import { Trader } from "./src/puppeteers/trader";
 import { ContractCode } from "./src/types/order";
 
 const init = async () => {
   await initSocketConnection();
+
+  const egsTrader = new Trader(
+    ContractCode.EGS_USDT,
+    EgsPriceFeedee,
+    EgsOrderFeedee
+  );
+  await egsTrader.init();
 
   const btcTrader = new Trader(
     ContractCode.BTC_USDT,
--- src/feedees/index.ts
+++ src/feedees/index.ts
@@ -1,8 +1,11 @@
 import AncOrderFeedee from "./orders/ancOrder";
 import BtcOrderFeedee from "./orders/btcOrder";
+import EgsOrderFeedee from "./orders/egsOrder";
 
 import AncPriceFeedee from "./prices/ancPrice";
 import BtcPriceFeedee from "./prices/btcPrice";
+import EgsPriceFeedee from "./prices/egsPrice";
 
-export const priceFeedees = [AncPriceFeedee, BtcPriceFeedee];
-export const orderFeedees = [AncOrderFeedee, BtcOrderFeedee];
+
+export const priceFeedees = [AncPriceFeedee, BtcPriceFeedee, EgsPriceFeedee];
+export const orderFeedees = [AncOrderFeedee, BtcOrderFeedee, EgsOrderFeedee];
