a0e9ba3b81b0594dadc0eb9ef4e42a6524b24f1c
witodivaro <witodivaro@gmail.com>
2022-05-08 22:19:43 +0300

    fix: remove egs

--- src/feedees/index.ts
+++ src/feedees/index.ts
@@ -1,11 +1,8 @@
 import AncOrderFeedee from "./orders/ancOrder";
 import BtcOrderFeedee from "./orders/btcOrder";
-import EgsOrderFeedee from "./orders/egsOrder";
 
 import AncPriceFeedee from "./prices/ancPrice";
 import BtcPriceFeedee from "./prices/btcPrice";
-import EgsPriceFeedee from "./prices/egsPrice";
 
-
-export const priceFeedees = [AncPriceFeedee, BtcPriceFeedee, EgsPriceFeedee];
-export const orderFeedees = [AncOrderFeedee, BtcOrderFeedee, EgsOrderFeedee];
+export const priceFeedees = [AncPriceFeedee, BtcPriceFeedee];
+export const orderFeedees = [AncOrderFeedee, BtcOrderFeedee];
