2314c45060769c242c8d9ac8abf9f800cb538190
witodivaro <witodivaro@gmail.com>
2022-05-11 15:19:54 +0300

    feat: increase stop loss dev to 4%

--- src/puppeteers/trader.data.ts
+++ src/puppeteers/trader.data.ts
@@ -1,4 +1,4 @@
-export const STOP_LOSS_DEVIATION = 3; // 3 (%)
+export const STOP_LOSS_DEVIATION = 4; // %
 
 export const LONG_STOP_LOSS_DEVIATION = -STOP_LOSS_DEVIATION;
 export const SHORT_STOP_LOSS_DEVIATION = +STOP_LOSS_DEVIATION;
